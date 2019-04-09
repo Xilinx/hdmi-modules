e96b835cf6df612087d707b421ffec11110029fd
Shikhar Mishra <shikhar.mishra@xilinx.com>
2019-04-09 13:42:36 +0800

    hdmitx:hdmirx:vphy: Support for auto DTG
    Code has been modified to support the auto generated
    device tree so that minimal modification is needed in the DT.
    
    Modification still needed after the implementation are
    - Adding si5324 and dp159 node (other board specific details not part
    				of hdf) in axi_iic node
    - Adding refhdmi clock node for si5324
    - Adding dru_clk in vphy which is also external
    - For hdcp design, adding hdmi_eeprom in axi_iic node
    - Changing the default value of "xlnx,vformat" in "v_drm_dmaengine_drv"
    to BG24 otherwise problem during first modeset
    - For hdcp design, reg-name feild needs to be added in RX and TX node
    to access the HDCP block key mngmnt block using reg name.
    
    Signed-off-by: Shikhar Mishra <shikhar.mishra@xilinx.com>

--- hdmi/xilinx_drm_hdmi.c
+++ hdmi/xilinx_drm_hdmi.c
@@ -2316,7 +2316,7 @@ static int xlnx_drm_hdmi_probe(struct platform_device *pdev)
 
 	/* 4 clock sources to be acquired and enabled */
 	/* acquire video streaming bus clock */
-	xhdmi->clk = devm_clk_get(xhdmi->dev, "video");
+	xhdmi->clk = devm_clk_get(xhdmi->dev, "s_axis_video_aclk");
 	if (IS_ERR(xhdmi->clk)) {
 		ret = PTR_ERR(xhdmi->clk);
 		if (ret == -EPROBE_DEFER)
@@ -2329,7 +2329,7 @@ static int xlnx_drm_hdmi_probe(struct platform_device *pdev)
 	clk_prepare_enable(xhdmi->clk);
 
 	/* acquire axi-lite register bus clock */
-	xhdmi->axi_lite_clk = devm_clk_get(xhdmi->dev, "axi-lite");
+	xhdmi->axi_lite_clk = devm_clk_get(xhdmi->dev, "s_axi_cpu_aclk");
 	if (IS_ERR(xhdmi->axi_lite_clk)) {
 		ret = PTR_ERR(xhdmi->clk);
 		if (ret == -EPROBE_DEFER)
@@ -2414,16 +2414,16 @@ static int xlnx_drm_hdmi_probe(struct platform_device *pdev)
 	}
 
 	if (xhdmi->config.Hdcp14.IsPresent) {
-	  xhdmi->hdcp1x_irq = platform_get_irq_byname(pdev, "hdcp1x");
+	  xhdmi->hdcp1x_irq = platform_get_irq_byname(pdev, "hdcp14_irq");
 	  dev_dbg(xhdmi->dev,"xhdmi->hdcp1x_irq = %d\n", xhdmi->hdcp1x_irq);
-	  xhdmi->hdcp1x_timer_irq = platform_get_irq_byname(pdev, "hdcp1x-timer");
+	  xhdmi->hdcp1x_timer_irq = platform_get_irq_byname(pdev, "hdcp14_timer_irq");
 	  dev_dbg(xhdmi->dev,"xhdmi->hdcp1x_timer_irq = %d\n", xhdmi->hdcp1x_timer_irq);
 	}
 
 	if (xhdmi->config.Hdcp22.IsPresent) {
-	  xhdmi->hdcp22_irq = platform_get_irq_byname(pdev, "hdcp22");
+	  xhdmi->hdcp22_irq = platform_get_irq_byname(pdev, "hdcp22_irq");
 	  dev_dbg(xhdmi->dev,"xhdmi->hdcp22_irq = %d\n", xhdmi->hdcp22_irq);
-	  xhdmi->hdcp22_timer_irq = platform_get_irq_byname(pdev, "hdcp22-timer");
+	  xhdmi->hdcp22_timer_irq = platform_get_irq_byname(pdev, "hdcp22_timer_irq");
 	  dev_dbg(xhdmi->dev,"xhdmi->hdcp22_timer_irq = %d\n", xhdmi->hdcp22_timer_irq);
 	}
 
